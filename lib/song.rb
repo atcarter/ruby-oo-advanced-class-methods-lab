e42bec5bc47d0bd9d1abd6fd001abdf267a78d0e
Antonio <atcarter005@gmail.com>
2021-01-10 14:47:18 -0600

    commit tree 3ccb5e7104f93c17246ed184b5430ed30e4c67d1

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,81 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    Song.all.find{|s|s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+
+    if find_by_name(name) == nil
+      create_by_name(name)
+
+    else
+      find_by_name(name)
+    end
+
+  end
+
+  def self.alphabetical
+    Song.all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(string)
+
+    array = string.split(" - ")
+    artist = array[0]
+    name = array[1][0..-5]
+    obj = self.new
+    obj.name = name
+    obj.artist_name = artist
+    obj
+
+  end
+
+  def self.create_from_filename(string)
+    array = string.split(" - ")
+    artist = array[0]
+    name = array[1][0..-5]
+    obj = self.new
+    obj.name = name
+    obj.artist_name = artist
+    @@all << obj
+
+  end
+
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
